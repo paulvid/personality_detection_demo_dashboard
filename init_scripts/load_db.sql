5f293edcb379ed74ccfef51cbe5d952c1b7f3c9e
Paul Vidal <paul.vid@gmail.com>
2018-12-15 14:29:11 -0500

    Reviewed DB_LOAD

--- init_scripts/load_db.sql
+++ init_scripts/load_db.sql
@@ -15,8 +15,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE admintle;
-use adminlte;
+CREATE DATABASE `admintle`;
+use `adminlte`;
 --
 -- Table structure for table `activity_logs`
 --
